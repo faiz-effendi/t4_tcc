1812fa99d3b8ea6d1728153f4ec51278d8976b72
faiz effendi <faizeffendi2004@gmail.com>
2025-03-06 21:36:27 +0700

    adjust 7

--- p2_frontend/src/components/navbar.jsx
+++ p2_frontend/src/components/navbar.jsx
@@ -9,11 +9,11 @@ function NavBar({ search, setSearch }) {
         <h1 className='text-2xl font-bold text-gray-600 md:inline hidden'>MyNotes</h1>
 
         {/* Search bar */}
-        <div className="relative w-96 flex-initial">
+        <div className="relative">
           <input
             type="text"
             placeholder="Search notes..."
-            className="pl-10 pr-4 py-2 border border-gray-600 rounded-md w-fill min-w-32 text-gray-600"
+            className="pl-10 pr-4 py-2 border border-gray-600 rounded-md w-96 flex-initial text-gray-600"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />

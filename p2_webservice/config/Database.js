1fae589dfe25d97a89d0d8bb9fd4a352f596d3d7
faiz effendi <faizeffendi2004@gmail.com>
2025-03-11 21:20:14 +0700

    fix: tuker vm ke vm-e07-2

--- p2_webservice/config/Database.js
+++ p2_webservice/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize"
 
-const dbContent = new Sequelize("t2_notes_123220139", "root", "", {
+const dbContent = new Sequelize("t2_notes_123220139", "root", "KorupsiTimah$300T", {
   host: "34.70.179.84",
   dialect: "mysql"
 });
